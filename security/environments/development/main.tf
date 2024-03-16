62fe2a950ba772e426245c694f2f9b6154f00da0
Voidray <team.yudin@gmail.com>
2024-03-16 14:23:26 -0500

    refactor into separate folders

--- security/environments/development/main.tf
+++ security/environments/development/main.tf
@@ -1,14 +1,3 @@
-module "budgets_dev" {
-  source = "../../modules/budgets"
-
-  name              = "monthly-budget-dev"
-  budget_type       = "COST"
-  limit_amount      = "101.0"
-  limit_unit        = "USD"
-  time_unit         = "MONTHLY"
-  time_period_start = "2024-03-16_00:01"
-}
-
 module "iam_budgets_dev" {
   source = "../../modules/iam"
 
